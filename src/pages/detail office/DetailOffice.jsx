0571e1251949e2e144829e78d143304976423762
BlackMoonC <rakelsera2001@gmail.com>
2022-06-08 19:52:08 +0700

    tambahan title overview

--- src/pages/detail office/DetailOffice.jsx
+++ src/pages/detail office/DetailOffice.jsx
@@ -4,7 +4,8 @@ export default function DetailOffice() {
   return (
     <div className="container">
     <div className="row">
-      <div className="col-7">
+      <div className="col-7 me-5">
+      <div>
       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -30,10 +31,61 @@ export default function DetailOffice() {
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+  </div>
+  <div className="mt-4">
+  <table class="table table-dark table-striped" style={{borderRadius:15}}>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+  <div>
+  <h2 style={{fontSize:24}}>Room Plan</h2>
+  </div>
+  <div>
+  <h2 style={{fontSize:24}}>Pricing</h2>
+  </div>
 </div>
       </div>
       <div className="col">
-        cakra
+        <div className="mb-4">
+          <h1 style={{fontSize: 32}}>BCA Tower</h1>
+          <p style={{fontSize: 24, color: 'grey'}}>50/F, Menara BCA Grand Indonesia, Jakarta, 10310</p>
+          <button type="button" class="btn btn-dark w-">Request Visit</button>
+        </div>
+        <div>
+          <h2 style={{fontSize:24}}>Overview</h2>
+          <p style={{fontSize: 20}}>Enjoy stunning views from the 50th floor of BCA Tower, located across the street from the famous Bundaran Hotel Indonesia (Bundaran HI). Widely regarded as the centre of Jakarta, the area offers excellent public transport and a supportive business environment.
+
+With its state-of-the-art design and first-class facilities, including swimming pool and penthouse restaurant, BCA Tower gives you all the benefits of the country’s rapid economic development. After a successful day, enjoy Jakarta’s numerous attractions, and take advantage of the building’s integrated shopping and entertainment complex.</p>
+        </div>
+        <div>
+        <h2 style={{fontSize:24}}>Location</h2>
+        </div>
       </div>
     </div>
     </div>
