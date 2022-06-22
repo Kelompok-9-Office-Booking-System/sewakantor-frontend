7c23a8d007fe48f83d5e0dc33b2a9cbdb69355e0
mbaharip <main@mbaharip.me>
2022-06-22 18:26:27 +0700

    Merge from user-landing

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,8 @@
 const routes = {
   home: "/",
-  
   register: "/register",
   login: "/login",
-  logout: "/logout",
-
   search: "/search",
-  discover: "/discover",
-  location: "/location",
-  contact: "/contact",
-
-  profile: "/profile",
-  booking: "/booking",
-  chat: "/chat",
 };
 
 export default routes;
