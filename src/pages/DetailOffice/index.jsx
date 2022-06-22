eef74f67e7642e571ed6449194f2283ed8570a56
BlackMoonC <rakelsera2001@gmail.com>
2022-06-22 20:34:10 +0700

    merapikan nav dan mengatur map

--- src/pages/DetailOffice/index.jsx
+++ src/pages/DetailOffice/index.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import '../DetailOffice/DetailOffice.css'
+// import icon react-icon
 import { BsFillStarFill} from "react-icons/bs";
+import { ImLocation } from "react-icons/im";
 
 export default function DetailOffice() {
   const [harga, setHarga] = useState("");
@@ -9,9 +11,9 @@ export default function DetailOffice() {
   // Make a stars
   const totalStar = [];
   for (let index = 0; index < 4 ; index++) {
-    totalStar.push( <BsFillStarFill className="mx-1" style={{color: '#FEC901'}}/>);        
+    totalStar.push( <BsFillStarFill className="mx-1" style={{color: '#FEC901'}}/>);         
   }
-  //fungsi tambah dan kurang
+  //fungsi tambah dan kurang quantity dan monthly
   const tambah = (tipe)=>{
     if(tipe === "quantity")
         setQuantity(quantity + 1);
@@ -86,11 +88,13 @@ const kurang = (tipe)=>{
   </tbody>
 </table>
   </div>
-  <div>
+  {/* Room Plan */}
+  <div className="mb-4">
   <h2>Room Plan</h2>
+  <img style={{height:400, width:'100%'}} src="https://images.unsplash.com/photo-1654795009861-c3fca8ccd055?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80" alt="..."/>
   </div>
   <div>
-  <h2>Pricing</h2>
+  <h2 className="">Pricing</h2>
   <div>
     <nav>
       <a href="#">Office Room</a>
@@ -99,7 +103,7 @@ const kurang = (tipe)=>{
       <a href='#'>Coworking</a>
     </nav>
   </div>
-  <div className="d-flex flex-row">
+  <div className="d-flex justify-content-between mt-4">
     <div>
       <label>Select Duration</label><br/>
       <select>
@@ -109,7 +113,7 @@ const kurang = (tipe)=>{
     </div>
     <div>
       <label>Select Quantity</label><br/>
-      <div className="d-flex flex-row">
+      <div className="d-flex justify-content-between">
         <button className="bg-yellow-400 px-1.5" onClick={() => kurang("quantity")}>-</button>
           <h2>{quantity}</h2>
         <button type="button" className="bg-yellow-400 px-1.5 " onClick={() => tambah("quantity")}>+</button>
@@ -117,7 +121,7 @@ const kurang = (tipe)=>{
     </div>
     <div>
       <label>No of Monthly</label><br/>
-      <div className="d-flex flex-row">
+      <div className="d-flex justify-content-between">
         <button className="bg-yellow-400 px-1.5" onClick={() => kurang("monthly")}>-</button>
           <h2>{monthly}</h2>
         <button type="button" className="bg-yellow-400 px-1.5 " onClick={() => tambah("monthly")}>+</button>
@@ -128,18 +132,18 @@ const kurang = (tipe)=>{
       <input type="date" name="date" />
     </div>
   </div>
-  <div className="d-flex flex-row">
+  <div className="d-flex justify-content-between mt-3">
     <p style={{fontWeight:'bold'}}>1 Month Selected</p>
     <button type="button" class="btn btn-dark">
       Check Avabililty
     </button>
   </div>
-  <div>
-    <h3>Rp {harga}</h3>
+  <div className="mb-4">
+    <h3>Rp 2.600.000</h3>
   </div>
   </div>
   <div>
-  <h2>They Said</h2>
+  <h2 className="mb-4">They Said</h2>
   <div className="row">
   <div className="card mb-3 col-4" style={{maxWidth: 540}}>
   <div className="row g-0">
@@ -197,7 +201,7 @@ const kurang = (tipe)=>{
         <div className="mb-4">
           <h1>BCA Tower</h1>
           <p  style={{color: 'grey'}}>50/F, Menara BCA Grand Indonesia, Jakarta, 10310</p>
-          <button type="button" class="btn btn-dark">Request Visit</button>
+          <button type="button" class="btn btn-dark px-5">Request Visit</button>
         </div>
         <div>
           <h2>Overview</h2>
@@ -206,20 +210,24 @@ const kurang = (tipe)=>{
 With its state-of-the-art design and first-class facilities, including swimming pool and penthouse restaurant, BCA Tower gives you all the benefits of the country’s rapid economic development. After a successful day, enjoy Jakarta’s numerous attractions, and take advantage of the building’s integrated shopping and entertainment complex.</p>
         </div>
         <div>
+        <div className="mb-4">
         <h2>Location</h2>
         {/* Maps */}
-        <img style={{width: '100%', height: '100%'}} src="https://images.unsplash.com/photo-1567004314453-ed46f03527fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=709&q=80"/>
-        {/* Nearby Location */}
+        <iframe style={{width: '100%', height: 300}} src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3986.90100633254!2d113.8835751143952!3d-2.1911853984028387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dfcb3c3c24d6357%3A0xf4f75e603e30a390!2sJl.%20Rajawali%20VII%2C%20Bukit%20Tunggal%2C%20Kec.%20Jekan%20Raya%2C%20Kota%20Palangka%20Raya%2C%20Kalimantan%20Tengah%2074874!5e0!3m2!1sen!2sid!4v1655901502512!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'></iframe>        {/* Nearby Location */}
+        </div>
         <div>
-          <h3>Nearby Places</h3>
+          <div className ="d-flex">
+            <ImLocation size={23}/>
+            <h3>Nearby Places</h3>
+          </div>
           <div>
-            <div className="d-flex">
+            <div className="d-flex justify-content-between">
               <div>
               <p>Gelora Bung Karno</p>
               </div>
               <p>0,1 km</p>
             </div>
-            <div className="d-flex">
+            <div className="d-flex justify-content-between">
               <div>
               <p>Gelora Bung Karno</p>
               </div>
@@ -232,16 +240,16 @@ With its state-of-the-art design and first-class facilities, including swimming 
         <h2>Facilities</h2>
         <div className="row">
           <div className="col-6">
-          <h3>Meeting Room</h3>
+          <p>Meeting Room</p>
           </div>
           <div className="col-6">
-            <h3>Free Area</h3>
+            <p>Free Area</p>
           </div>
           <div className="col-6">
-          <h3>Meeting Room</h3>
+          <p>Meeting Room</p>
           </div>
           <div className="col-6">
-            <h3>Free Area</h3>
+            <p>Free Area</p>
           </div>
         </div>
         </div>
