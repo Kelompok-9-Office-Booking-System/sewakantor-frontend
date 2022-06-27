1edfcc03703721eb1799b9636a2ea09846306f9e
mbaharip <main@mbaharip.me>
2022-06-27 19:33:42 +0700

    Add BrowserRouter to App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 // Pages
 import App from "./App";
@@ -14,6 +15,8 @@ import "./style/custom.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
