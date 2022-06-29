2779b6e6e6f3ef2f91c29052cde5eb9746d336cf
mbaharip <main@mbaharip.me>
2022-06-29 19:53:58 +0700

    Merge complete component and pages

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 // React
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Style
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/custom.css';
-import 'bootstrap/dist/js/bootstrap';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/custom.css";
+import "bootstrap/dist/js/bootstrap";
 
 // Pages
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<App />} />
-			</Routes>
-		</BrowserRouter>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
