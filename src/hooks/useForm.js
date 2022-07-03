26c2fb740d71a61a8d9b3707f714d96d1a00392d
Melysa Anggun <melysa19ti@mahasiswa.pcr.ac.id>
2022-07-03 17:46:54 +0700

    Add axios post when sign up

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
+
+const REGISTER_URL = "http://54.211.120.43/api/v1/customer/auth/register";
 
 const useForm = validate => {
   const [values, setValues] = useState({
@@ -18,19 +21,43 @@ const useForm = validate => {
     })
   }
 
-  const handleSubmit = e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     setErrors(validate(values));
+
+    try {
+      const response = await axios.post(
+        REGISTER_URL,
+        {
+          "email": values.email,
+          "password": values.password,
+          "firstName": values.firstname,
+          "lastName": values.lastname
+        },
+        {
+          headers: {
+            Authorization:
+            'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqaG9uZG9lQG1haWwuY29tIiwiZXhwIjoxNjU2MzM1MzA2LCJpYXQiOjE2NTYzMTczMDZ9.zRnaXeVa9k4cIKslLpPDckBOibkVMbLgb2rpdkJMbH1fKPNZmLMccSj4PvGeZi4Q-x0-SPXZcrz8PvuQm7fyRA',
+          }
+        })
+        console.log(response.data)
+    } catch (err) {
+      if(!err.response) {
+        setErrors('No Server Response');
+      } else if (err.response?.status === 409) {
+        setErrors('Username Taken');
+      } else {
+        setErrors('Registration Failed')
+      }
+    }
   }
 
   useEffect(() => {
     setErrors('');
   }, [values])
 
-  // useEffect(() => {
-  //   if (Object.keys(errors).length === 0 )
-  // })
+  
 
   return { handleChange, values, handleSubmit, errors};
 };
