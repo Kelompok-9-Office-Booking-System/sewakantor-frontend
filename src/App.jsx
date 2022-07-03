39d41f33f16990b532c8d19fb57073d59a6ecf6f
mbaharip <main@mbaharip.me>
2022-07-04 06:09:47 +0700

    Add 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import UserFooter from "./components/UserFooter";
 import UserNavbar from "./components/UserNavbar";
+import NotFound from "./pages/NotFound";
 import SearchOffice from "./pages/SearchOffice";
 import UserLanding from "./pages/UserLanding";
 import UserRegis from "./pages/UserRegis";
@@ -10,19 +11,21 @@ import routes from "./routes";
 
 function App() {
   const location = useLocation();
+  const includeNav = [routes.home, routes.search, routes.discover];
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
   return (
     <>
-      <UserNavbar />
+      {includeNav.indexOf(location.pathname) > -1 && <UserNavbar />}
       <Routes>
         <Route path={routes.home} element={<UserLanding />} />
         <Route path={routes.register} element={<UserRegis />} />
         <Route path={routes.search} element={<SearchOffice />} />
         <Route path={routes.discover} element={<SearchOffice />} />
+        <Route path={"*"} element={<NotFound />} />
       </Routes>
-      <UserFooter />
+      {includeNav.indexOf(location.pathname) > -1 && <UserFooter />}
     </>
   );
 }
