6a575b8ec3101a43b1829024e8cc131ce3e30362
mbaharip <main@mbaharip.me>
2022-07-11 19:27:31 +0700

    Create content container component for admin

---
+++ src/components/AdminContentContainer/index.jsx
@@ -0,0 +1,18 @@
+import AdminBreadcrumb from "../AdminBreadcrumb";
+import AdminTopbar from "../AdminTopbar";
+import style from "./style.module.css";
+
+const AdminContentContainer = ({ title = "", breadcrumb = true, children }) => {
+  return (
+    <div className={`${style.contentContainer} d-flex flex-column`}>
+      <AdminTopbar />
+      <div className={`${style.headerContent}`}>
+        <h1 className={style.headerTitle}>{title}</h1>
+        {breadcrumb && <AdminBreadcrumb />}
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default AdminContentContainer;
