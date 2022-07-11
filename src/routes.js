4340accccdf720a7da0e7a516c42415cdf1261db
mbaharip <main@mbaharip.me>
2022-07-11 19:28:31 +0700

    Add admin routes

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,32 @@ const routes = {
   profile: "/profile",
   booking: "/booking",
   chat: "/chat",
+
+  adminDashboard: "/admin/dashboard",
+  adminLogin: "/admin/login",
+  adminLogout: "/admin/logout",
+
+  adminBookings: "/admin/bookings",
+  adminBookingsAdd: "/admin/bookings/add",
+  adminBookingsEdit: "/admin/bookings/edit/:id",
+
+  adminSpaces: "/admin/spaces",
+  adminSpacesAdd: "/admin/spaces/add",
+  adminSpacesEdit: "/admin/spaces/edit/:id",
+
+  adminUsers: "/admin/users",
+  adminUsersAdd: "/admin/users/add",
+  adminUsersEdit: "/admin/users/edit/:id",
+
+  adminManagers: "/admin/managers",
+  adminManagersAdd: "/admin/managers/add",
+  adminManagersEdit: "/admin/managers/edit/:id",
+
+  adminReviews: "/admin/reviews",
+  adminReviewsAdd: "/admin/reviews/add",
+  adminReviewsEdit: "/admin/reviews/edit/:id",
+
+  adminLivechat: "/admin/livechat",
 };
 
 export default routes;
