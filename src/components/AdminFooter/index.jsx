d7e8454d565c4efa4b80e55c4c85e77a0ffea3d9
Melysa Anggun <melysa19ti@mahasiswa.pcr.ac.id>
2022-07-16 00:05:46 +0700

    Slicing spaces admin page

---
+++ src/components/AdminFooter/index.jsx
@@ -0,0 +1,7 @@
+import style from "./style.module.css";
+
+const AdminFooter = () => {
+  return <div className={style.footerContainer}>&copy; Sewakantor 2022</div>;
+};
+
+export default AdminFooter;
