147410b35b8ded30f05915487fa6ff236bf9bce3
BlackMoonC <rakelsera2001@gmail.com>
2022-07-22 02:05:02 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,13 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import "./App.css";
-import AdminContentContainer from "./components/AdminContentContainer";
-import AdminDataContainer from "./components/AdminDataContainer";
-import AdminDataTable from "./components/AdminDataTable";
-import AdminFooter from "./components/AdminFooter";
-import AdminSidebar from "./components/AdminSidebar";
-
-const placeholderBuilding = [
-  {
-    id: 1,
-    thumbnail: "https://placeholder.pics/svg/280x175",
-    towerName: "BCA Tower Lorem",
-    units: (Math.random() * 1000).toFixed(0),
-    ratings: [5, 4, 4, 4, 1, 5, 5, 5, 4],
-    address: "50/F, Menara BCA Grand Indonesia, Jakarta, 10310",
-    price: 1700000,
-  },
-  {
-    id: 2,
-    thumbnail: "https://placeholder.pics/svg/280x175",
-    towerName: "BCA Tower Ipsum",
-    units: (Math.random() * 1000).toFixed(0),
-    ratings: [5, 4, 4, 4, 1, 5, 5, 5, 4],
-    address: "50/F, Menara BCA Grand Indonesia, Bandung, 10310",
-    price: 2700000,
-  },
-  {
-    id: 3,
-    thumbnail: "https://placeholder.pics/svg/280x175",
-    towerName: "BCA Tower Dolor",
-    units: (Math.random() * 1000).toFixed(0),
-    ratings: [5, 4, 4, 4, 1, 5, 5, 5, 4],
-    address: "50/F, Menara BCA Grand Indonesia, Tangerang, 10310",
-    price: 3700000,
-  },
-  {
-    id: 4,
-    thumbnail: "https://placeholder.pics/svg/280x175",
-    towerName: "BCA Tower Sit",
-    units: (Math.random() * 1000).toFixed(0),
-    ratings: [5, 4, 4, 4, 1, 5, 5, 5, 4],
-    address: "50/F, Menara BCA Grand Indonesia, Bekasi, 10310",
-    price: 4000000,
-  },
-  {
-    id: 5,
-    thumbnail: "https://placeholder.pics/svg/280x175",
-    towerName: "BCA Tower Amet",
-    units: (Math.random() * 1000).toFixed(0),
-    ratings: [5, 4, 4, 4, 1, 5, 5, 5, 4],
-    address: "50/F, Menara BCA Grand Indonesia, Jakarta, 10310",
-    price: 5000000,
-  },
-];
+import AddBooking from './pages/AddBooking'
 
 function App() {
-  const location = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
 
   return (
-      <div
-        className={`bg-skSmoke position-relative`}
-        style={{ height: "100%" }}
-      >
-        <AdminSidebar />
-        <AdminContentContainer title={"Add Schedule Booking"} breadcrumb={true}>
-
-        </AdminContentContainer>
-        <AdminFooter />
-      </div>
+      <>
+        <AddBooking/>
+      </>
   );
 }
 
